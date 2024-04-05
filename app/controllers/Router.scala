b0bb9145d0fc18758b81c92d897f0f70dd8de9c5
weiraneve <907221539@qq.com>
2024-04-05 16:59:23 +0800

    feat: add router

---
+++ app/controllers/Router.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+import play.api.routing.sird.*
+
+import javax.inject.Inject
+
+class Router @Inject()(controller: HomeController) extends SimpleRouter {
+
+  override def routes: Routes = {
+    case GET(p"/") =>
+      controller.index()
+  }
+}
